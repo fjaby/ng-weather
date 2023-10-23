d0a7ba94d0ba89c5f603abffad35a31f04c27926
Florian <zenattitude.fj@gmail.com>
2023-10-23 23:07:39 +0200

    update url for openweathermap

--- docs/main.js
+++ docs/main.js
@@ -367,7 +367,7 @@ var WeatherService_1;
 
 
 let WeatherService = WeatherService_1 = class WeatherService {
-  static #_ = this.URL = 'http://api.openweathermap.org/data/2.5';
+  static #_ = this.URL = 'https://api.openweathermap.org/data/2.5';
   static #_2 = this.APPID = '5a4b2d457ecbef9eb2a71e480b947604';
   static #_3 = this.ICON_URL = 'https://raw.githubusercontent.com/udacity/Sunshine-Version-2/sunshine_master/app/src/main/res/drawable-hdpi/';
   constructor(http) {
